e207191487f4fb3ce506e4cd7ade559cca96401d
coriolang <yan.radi@gmail.com>
2022-08-30 16:39:56 +0300

    Responding at http://localhost:8080/ path

--- src/main/kotlin/com/coriolang/Application.kt
+++ src/main/kotlin/com/coriolang/Application.kt
@@ -3,9 +3,22 @@ package com.coriolang
 import com.coriolang.data.DatabaseSettings
 import io.ktor.server.application.*
 import com.coriolang.plugins.*
+import io.ktor.network.tls.certificates.*
+import java.io.File
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
+
+// generate keystore file
+//fun main(args: Array<String>) {
+//    val keyStoreFile = File("build/keystore.jks")
+//    val keystore = generateCertificate(
+//        file = keyStoreFile,
+//        keyAlias = "sampleAlias",
+//        keyPassword = "foobar",
+//        jksPassword = "foobar"
+//    )
+//}
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
